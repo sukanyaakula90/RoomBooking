abd2ca8862fd88ca71279726e242558e487f19a5
sukanya <sukanyaakula90@gmail.com>
2023-08-24 14:55:52 +0530

    Updated two classes login and registration

--- src/main/java/com/example/roombooking/RoomLogin.java
+++ src/main/java/com/example/roombooking/RoomLogin.java
@@ -3,5 +3,9 @@ package com.example.roombooking;
 public class RoomLogin {
     public static void main(String[] args) {
        System.out.println("Room login successful");
+        System.out.println("Room login successful");
+        System.out.println("Room login successful");
+        System.out.println("Room login successful");
+
     }
 }
--- src/main/java/com/example/roombooking/RoomRegistration.java
+++ src/main/java/com/example/roombooking/RoomRegistration.java
@@ -4,5 +4,6 @@ public class RoomRegistration {
 
     public static void main(String[]args){
         System.out.println("Booking sucessfull");
+
     }
 }
